f4e515d0bf3a70971c66ad86280fb2266d63fae3
JS <nova5555@gmail.com>
2019-10-04 13:05:41 -0400

    Added a lot of stuff

--- Hexagon.js/hexagon.js
+++ Hexagon.js/hexagon.js
@@ -1,10 +1,13 @@
 // Hex math defined here: http://blog.ruslans.com/2011/02/hexagonal-grid-math.html
 
+//#region Object definitions
 class HexObject {
-    constructor(image, PointF){
+    constructor(image, PointF, encType){
         this._Image = image;
         this._IsSelected = false;
         this._PointF = PointF;
+        this._IsVisible = false;
+        this._EncounterType = encType;
     }
     //#region Properties
 
@@ -29,6 +32,19 @@ class HexObject {
         this._Image = value;
     }
 
+    get IsVisible(){
+        return this._IsVisible;
+    }
+    set IsVisible(value){
+        this._IsVisible = value;
+    }
+
+    get EncounterType(){
+        return this._EncounterType;
+    }
+    set EncounterType(value){
+        this._EncounterType = value;
+    }
     //#endregion 
 }
 
@@ -36,6 +52,10 @@ class PointF {
     constructor(x,y){
         this._Row = x;
         this._Col = y;
+    }
+
+    get Points(){
+        return { row: this._Row, column: this._Col}
     }
 
     get Row(){
@@ -52,6 +72,42 @@ class PointF {
     }
 }
 
+class PlayerCharacter {
+    constructor(str,health,dmg,tohit){
+        this._Strength = str;
+        this._Health = health;
+        this._Damage = dmg;
+        this._ToHit = tohit;
+    }
+
+    get Strength(){
+        return this._Strength;
+    }
+    set Strength(value){
+        this._Strength = value;
+    }
+    get Health(){
+        return this._Health;
+    }
+    set Health(value){
+        this._Health = value;
+    }
+    get Damage(){
+        return this._Damage;
+    }
+    set Damage(value){
+        return this._Damage = value;
+    }
+    get ToHit(){
+        return this._ToHit;
+    }
+    set ToHit(value){
+        return this._ToHit = value;
+    }
+}
+//#endregion 
+
+//#region Enums
 const ModeTypes = {
     PLACEMENT: 'placement',
     SELECTION: 'selection',
@@ -61,10 +117,24 @@ const ModeTypes = {
     INSPECT: 'inspect'
 }
 
+const EncounterTypes = {
+    COMBAT: 'combat',
+    TREASURE: 'treasure',
+    REST: 'rest',
+    FRIENDLY: 'friendly',
+    INFORMATION: 'information'
+}
+//#endregion 
+
+//#region Global Variables
+var PlayerParty = [];
 var c = document.getElementById("HexCanvas");
 var ctx = c.getContext("2d");
 var HexObjects = [];
 let CurrentModeType = ModeTypes.DEFAULT;
+SelectedHex = new PointF;
+var Hexes = [];
+//#endregion 
 
 function HexagonGrid(canvasId, radius) {
     this.radius = radius;
@@ -80,8 +150,9 @@ function HexagonGrid(canvasId, radius) {
     this.canvasOriginY = 0;
     
     this.canvas.addEventListener("mousedown", this.clickEvent.bind(this), false);
-};
+}
 
+//#region HexagonGrid
 HexagonGrid.prototype.drawHexGrid = function (rows, cols, originX, originY, isDebug) {
     this.canvasOriginX = originX;
     this.canvasOriginY = originY;
@@ -111,14 +182,13 @@ HexagonGrid.prototype.drawHexGrid = function (rows, cols, originX, originY, isDe
         }
         offsetColumn = !offsetColumn;
     }
-};
+}
 
 HexagonGrid.prototype.drawHexAtColRow = function(column, row, color, image) {
     var drawy = column % 2 == 0 ? (row * this.height) + this.canvasOriginY : (row * this.height) + this.canvasOriginY + (this.height / 2);
     var drawx = (column * this.side) + this.canvasOriginX;
-
     this.drawHex(drawx, drawy, color, "", image);
-};
+}
 
 HexagonGrid.prototype.drawHex = function(x0, y0, fillColor, debugText,image) {
     this.context.strokeStyle = "#000";
@@ -147,7 +217,8 @@ HexagonGrid.prototype.drawHex = function(x0, y0, fillColor, debugText,image) {
         this.context.fillStyle = "#000";
         this.context.fillText(debugText, x0 + (this.width / 2) - (this.width/4), y0 + (this.height - 5));
     }
-};
+    Hexes.push(new HexObject(image,this.getSelectedTile(x0,y0)));
+}
 
 //Recusivly step up to the body to calculate canvas offset.
 HexagonGrid.prototype.getRelativeCanvasOffset = function() {
@@ -233,16 +304,16 @@ HexagonGrid.prototype.getSelectedTile = function(mouseX, mouseY) {
     }
 
     return  { row: row, column: column };
-};
+}
 
 HexagonGrid.prototype.IdentifyHexContents = function(mouseX, mouseY){
     var tile = this.getSelectedTile(mouseX, mouseY);
-    console.log(tile);
+    return ContainsUnit(tile);
 }
 
 HexagonGrid.prototype.sign = function(p1, p2, p3) {
     return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-};
+}
 
 //TODO: Replace with optimized barycentric coordinate method
 HexagonGrid.prototype.isPointInTriangle = function isPointInTriangle(pt, v1, v2, v3) {
@@ -253,7 +324,7 @@ HexagonGrid.prototype.isPointInTriangle = function isPointInTriangle(pt, v1, v2,
     b3 = this.sign(pt, v3, v1) < 0.0;
 
     return ((b1 == b2) && (b2 == b3));
-};
+}
 
 HexagonGrid.prototype.clickEvent = function (e) {
     var mouseX = e.pageX;
@@ -262,37 +333,117 @@ HexagonGrid.prototype.clickEvent = function (e) {
     var localX = mouseX - this.canvasOriginX;
     var localY = mouseY - this.canvasOriginY;
 
-    this.IdentifyHexContents(localX,localY);
-
     var tile = this.getSelectedTile(localX, localY);
+    alert(tile.column);
+    /*
+    if (this.IdentifyHexContents(localX,localY)){
+        var unitIndex = this.GetUnitIndexAtSelection(localX,localY);
+        console.log(unitIndex);
+        console.log(CurrentModeType);
+        if (CurrentModeType == ModeTypes.SELECTION){
+            if (unitIndex != "Not Found"){
+                HexObjects[unitIndex].IsSelected = true;
+                CurrentModeType = ModeTypes.MOVEMENT;
+            }
+        }
+        if (CurrentModeType == ModeTypes.MOVEMENT){
+            HexObjects[unitIndex].PointF = tile;
+        }
+    }
+    */
+
     if (tile.column >= 0 && tile.row >= 0) {
         var drawy = tile.column % 2 == 0 ? (tile.row * this.height) + this.canvasOriginY + 6 : (tile.row * this.height) + this.canvasOriginY + 6 + (this.height / 2);
         var drawx = (tile.column * this.side) + this.canvasOriginX;
 
         //this.drawHex(drawx, drawy - 6, "rgba(110,110,70,0.3)", "");
-        this.drawHex(drawx, drawy - 6,"","","");
+        //this.drawHex(drawx, drawy - 6,"","","");
     } 
-};
-
-function InitializeBoardData(){
-    var image = new Image(20,20);
-    image.src = 'images/1.png';
-    HexObjects.push(new HexObject(image,new PointF(0,0)));
-    image = new Image(20,20);
-    image.src = 'images/2.png';
-    HexObjects.push(new HexObject(image,new PointF(0,1)));
-    image = new Image(20,20);
-    image.src = 'images/3.png';
-    HexObjects.push(new HexObject(image,new PointF(0,2)));
-    image = new Image(20,20);
-    image.src = 'images/4.png';
-    HexObjects.push(new HexObject(image,new PointF(0,3)));
 }
 
+HexagonGrid.prototype.GetUnitIndexAtSelection = function(mouseX, mouseY){
+    var tile = this.getSelectedTile(mouseX, mouseY);
+    for (index = 0; index < HexObjects.length;index++){
+        if (_.isEqual(tile,HexObjects[index].PointF.Points)){
+            return index;
+            break;
+        }
+    }
+    return "Not Found";
+}
+
+HexagonGrid.prototype.DetermineEncounter = function(mouseX, mouseY){
+    var tile = this.getSelectedTile(mouseX, mouseY);
+}
+//#endregion
+
+function ContainsUnit(pointF){
+    for (index = 0; index < HexObjects.length;index++){
+        if (_.isEqual(pointF,HexObjects[index].PointF.Points)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function InitializeGameData(){
+    /*
+    var image = new Image(20,20);
+    image.src = 'images/1.png';
+    var HexObj = new HexObject(image,new PointF(0,0),EncounterTypes.COMBAT);
+    HexObj.IsVisible = true;
+    HexObjects.push(HexObj);
+    image = new Image(20,20);
+    image.src = 'images/2.png';
+    HexObjects.push(new HexObject(image,new PointF(0,1),EncounterTypes.TREASURE));
+    image = new Image(20,20);
+    image.src = 'images/3.png';
+    HexObjects.push(new HexObject(image,new PointF(0,2),EncounterTypes.REST));
+    image = new Image(20,20);
+    image.src = 'images/4.png';
+    HexObjects.push(new HexObject(image,new PointF(0,3),EncounterTypes.FRIENDLY));
+    */
+
+    for (index = 0; index < Hexes.length;index++){
+        Hexes[index].EncounterType = RandomEncounter();
+    }
+    PlayerParty.push(new PlayerCharacter(10,20,5,10));
+}
+
+function RandomEncounter(){
+    rnd = Math.ceil(Math.random() * 5);
+    switch(rnd){
+        case 1:
+            return EncounterTypes.COMBAT;
+            break;
+        case 2:
+            return EncounterTypes.FRIENDLY;
+            break;
+        case 3:
+            return EncounterTypes.INFORMATION;
+            break;
+        case 4:
+            return EncounterTypes.REST;
+            break;
+        case 5:
+            return EncounterTypes.TREASURE;
+            break;
+        default:
+            return EncounterTypes.COMBAT;
+    }
+}
+
+//#region Button functions
 function LoadBoard(HexagonGrid){
     for (index = 0; index < HexObjects.length;index++){
-        HexagonGrid.drawHexAtColRow(HexObjects[index].PointF.Col,HexObjects[index].PointF.Row,"",HexObjects[index].Image);
+        if (HexObjects[index].IsVisible){
+            HexagonGrid.drawHexAtColRow(HexObjects[index].PointF.Col,HexObjects[index].PointF.Row,"",HexObjects[index].Image);
+        }
     }
+
+    console.log(Hexes.length);
+    console.log(Hexes[0].PointF);
+    console.log(Hexes[0].EncounterType);
 }
 
 function ClearBoard(HexagonGrid){
@@ -324,7 +475,5 @@ function SetSelectionMode(Mode){
         default:
             CurrentModeType = ModeTypes.DEFAULT;
     }
-
-    console.log(CurrentModeType);
-
 }
+//#endregion
